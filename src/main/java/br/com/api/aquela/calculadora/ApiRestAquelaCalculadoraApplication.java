c441e790a0647771b8c49ab3b5049f693cc21524
Mah <marinalarissacarpesrohrig@gmail.com>
2023-02-13 19:21:24 -0300

    spring boot init

---
+++ src/main/java/br/com/api/aquela/calculadora/ApiRestAquelaCalculadoraApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.aquela.calculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestAquelaCalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestAquelaCalculadoraApplication.class, args);
+	}
+
+}
